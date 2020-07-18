be46aa4e4ce1abdd1c067adc3d094dd3afca6908
Arun Singh <arun@Aruns-MacBook-Pro.local>
2020-07-18 16:32:04 -0700

    Add items to the table and retrieve the value

--- src/main/java/com/arun/springdynamodbdockerlocal/controller/TokenController.java
+++ src/main/java/com/arun/springdynamodbdockerlocal/controller/TokenController.java
@@ -1,15 +1,11 @@
 package com.arun.springdynamodbdockerlocal.controller;
 
-import com.arun.springdynamodbdockerlocal.model.request.TokenRequest;
 import com.arun.springdynamodbdockerlocal.repository.TokenValidationRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author arun on 7/18/20
@@ -24,7 +20,7 @@ public class TokenController {
     }
 
     @GetMapping("/v1/token/{actorId}")
-    public ResponseEntity<HttpStatus> getToken(@PathVariable String actorId, @RequestBody List<TokenRequest> tokenRequests) {
+    public ResponseEntity<HttpStatus> getToken(@PathVariable String actorId) {
         tokenValidationRepository.getToken(actorId);
         return ResponseEntity.ok(HttpStatus.ACCEPTED);
     }
--- src/main/java/com/arun/springdynamodbdockerlocal/repository/TokenValidationRepository.java
+++ src/main/java/com/arun/springdynamodbdockerlocal/repository/TokenValidationRepository.java
@@ -1,14 +1,14 @@
 package com.arun.springdynamodbdockerlocal.repository;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
+import com.amazonaws.services.dynamodbv2.model.*;
 import com.arun.springdynamodbdockerlocal.config.AwsConfig;
+import com.google.common.collect.ImmutableMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,17 +26,49 @@ public class TokenValidationRepository {
         this.amazonDynamoDB = amazonDynamoDB;
     }
 
-    public void getToken(String actorId){
+    public void getToken(String actorId) {
 
-        Map<String, AttributeValue> itemKey = new HashMap<>();
-        itemKey.put("customerId", new AttributeValue().withS(actorId));
+        QueryRequest queryRequest = new QueryRequest();
+        queryRequest.withTableName(awsConfig.getTableName())
+                .withKeyConditions(ImmutableMap.of("actorId", new Condition()
+                        .withComparisonOperator(ComparisonOperator.EQ)
+                        .withAttributeValueList(new AttributeValue(actorId))));
 
-        GetItemRequest getItemRequest = new GetItemRequest()
-                .withTableName(awsConfig.getTableName())
-                .withKey(itemKey)
-                .withConsistentRead(true);
 
-        GetItemResult item = amazonDynamoDB.getItem(getItemRequest);
+        QueryResult query = amazonDynamoDB.query(queryRequest);
+
+        List<Map<String, AttributeValue>> items = query.getItems();
+        if (items.isEmpty()) {
+            putToken(actorId);
+        }
+
+        items.forEach(item -> {
+            AttributeValue actor = item.get("actorId");
+            System.out.println(actor.getS());
+            AttributeValue duration = item.get("duration");
+            System.out.println(duration.getS());
+            AttributeValue count = item.get("count");
+            System.out.println(count.getN());
+        });
+
+    }
+
+    public void putToken(String actorId) {
+        Map<String, AttributeValue> item24Hr = new HashMap<>();
+        Map<String, AttributeValue> item30Day = new HashMap<>();
+
+        item24Hr.put("actorId", new AttributeValue().withS(actorId));
+        item24Hr.put("duration", new AttributeValue().withS("24 hrs"));
+        item24Hr.put("count", new AttributeValue().withN("1"));
+
+        item30Day.put("actorId", new AttributeValue().withS(actorId));
+        item30Day.put("duration", new AttributeValue().withS("30 days"));
+        item30Day.put("count", new AttributeValue().withN("1"));
+
+        PutItemRequest putItemFor24Hr = new PutItemRequest(awsConfig.getTableName(), item24Hr);
+        PutItemRequest putItemFor30Days = new PutItemRequest(awsConfig.getTableName(), item30Day);
+        amazonDynamoDB.putItem(putItemFor24Hr);
+        amazonDynamoDB.putItem(putItemFor30Days);
     }
 
 
