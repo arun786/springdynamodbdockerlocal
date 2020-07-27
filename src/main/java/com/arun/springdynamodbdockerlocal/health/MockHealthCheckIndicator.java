9bc1b6eb06408ca0386fe1653d2aa1d3250f35e3
Arun Singh <arun@Aruns-MacBook-Pro.local>
2020-07-26 21:56:18 -0700

    Health check for mock service wich runs on a different port

--- src/main/java/com/arun/springdynamodbdockerlocal/health/MockHealthCheckIndicator.java
+++ src/main/java/com/arun/springdynamodbdockerlocal/health/MockHealthCheckIndicator.java
@@ -1,13 +1,14 @@
 package com.arun.springdynamodbdockerlocal.health;
 
 import com.arun.springdynamodbdockerlocal.config.UrlConfig;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -18,7 +19,8 @@ import java.util.Map;
  * @author arun on 7/25/20
  */
 
-//@Component
+@Component
+@Slf4j
 public class MockHealthCheckIndicator implements HealthIndicator {
 
     private final WebClient webClient;
@@ -36,22 +38,19 @@ public class MockHealthCheckIndicator implements HealthIndicator {
 
     @Override
     public Health health() {
-        Mono<JsonNode> jsonNodeMono = webClient.method(HttpMethod.GET).uri(URI.create(urlConfig.getMockActuatorUrl()))
-                .retrieve().bodyToMono(JsonNode.class);
-
-
-        JsonNode jsonNode = jsonNodeMono.blockOptional().orElseThrow();
-        ObjectMapper objectMapper = new ObjectMapper();
+        Health mockHealth = Health.down().build();
         try {
+            Mono<JsonNode> jsonNodeMono = webClient.method(HttpMethod.GET).uri(URI.create(urlConfig.getMockActuatorUrl()))
+                    .retrieve().bodyToMono(JsonNode.class);
+            JsonNode jsonNode = jsonNodeMono.blockOptional().orElseThrow();
+            ObjectMapper objectMapper = new ObjectMapper();
             Map<String, String> valueStatus = objectMapper.readValue(String.valueOf(jsonNode), new TypeReference<>() {
             });
 
-            return valueStatus.get("status").equals("UP") ? Health.up().build() : Health.down().build();
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            return valueStatus.get("status").equals("UP") ? Health.up().build() : mockHealth;
+        } catch (Exception e) {
+            log.error("Mock service is {}", mockHealth);
+            return mockHealth;
         }
-
-
-        return Health.down().build();
     }
 }
