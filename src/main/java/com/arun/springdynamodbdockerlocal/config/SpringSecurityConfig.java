712012659ff047e2eddbf76ebaa3d971f0c48bf3
Arun Singh <arun@Aruns-MacBook-Pro.local>
2020-07-26 18:51:39 -0700

    Basic security implemented

--- src/main/java/com/arun/springdynamodbdockerlocal/config/SpringSecurityConfig.java
+++ src/main/java/com/arun/springdynamodbdockerlocal/config/SpringSecurityConfig.java
@@ -1,15 +1,23 @@
 package com.arun.springdynamodbdockerlocal.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
 /**
  * @author arun on 7/25/20
  */
-//@Configuration
-//@EnableConfigurationProperties
-//@ConfigurationProperties(prefix = "spring.security.user")
-//@Getter
-//@Setter
-//public class SpringSecurityConfig {
-//    private String name;
-//    private String password;
-//    private List<String> roles;
-//}
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "spring.security.user")
+@Getter
+@Setter
+public class SpringSecurityConfig {
+    private String name;
+    private String password;
+    private List<String> roles;
+}
--- src/main/java/com/arun/springdynamodbdockerlocal/securityconfig/BasicConfiguration.java
+++ src/main/java/com/arun/springdynamodbdockerlocal/securityconfig/BasicConfiguration.java
@@ -1,59 +1,46 @@
-//package com.arun.springdynamodbdockerlocal.securityconfig;
-//
-//import com.arun.springdynamodbdockerlocal.config.SpringSecurityConfig;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//import java.util.List;
-//
-///**
-// * @author arun on 7/25/20
-// */
-//@Configuration
-//@EnableWebSecurity
-//public class BasicConfiguration extends WebSecurityConfigurerAdapter {
-//    private final SpringSecurityConfig springSecurityConfig;
-//
-//    @Autowired
-//    public BasicConfiguration(SpringSecurityConfig springSecurityConfig) {
-//        this.springSecurityConfig = springSecurityConfig;
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        String userName = springSecurityConfig.getName();
-//        String password = springSecurityConfig.getPassword();
-//        List<String> roles = springSecurityConfig.getRoles();
-//        String userRole = roles.get(0);
-//        String adminRole = roles.get(1);
-//
-//
-//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser(userName)
-//                .password(encoder.encode(password))
-//                .roles(userRole)
-//                .and()
-//                .withUser("admin")
-//                .password(encoder.encode(password))
-//                .roles(userRole, adminRole);
-//
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
-//    }
-//}
+package com.arun.springdynamodbdockerlocal.securityconfig;
+
+import com.arun.springdynamodbdockerlocal.config.SpringSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author arun on 7/25/20
+ */
+@Configuration
+@EnableWebSecurity
+public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+    private final SpringSecurityConfig springSecurityConfig;
+
+    @Autowired
+    public BasicConfiguration(SpringSecurityConfig springSecurityConfig) {
+        this.springSecurityConfig = springSecurityConfig;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(springSecurityConfig.getName())
+                .password("{noop}" + springSecurityConfig.getPassword())
+                .roles(springSecurityConfig.getRoles().get(0), springSecurityConfig.getRoles().get(1));
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/v1/**").hasRole(springSecurityConfig.getRoles().get(0))
+                .antMatchers("/actuator/**").hasRole(springSecurityConfig.getRoles().get(1))
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
